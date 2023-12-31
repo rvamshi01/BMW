2fca9e83e857355110fb9bce4c5369353934d53d
rvamshi01 <146413004+rvamshi01@users.noreply.github.com>
2023-12-31 16:07:55 +0530

    Add files via upload

---
+++ src/components/CardAxios.js
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import Getproductsapi from "../api/Getproducts";
+import { Link } from "react-router-dom";
+// import axios from 'axios'
+
+export default function CardAxios() {
+  const [search, setSearch] = useState("");
+  const [products, setProducts] = useState([]);
+
+  const getproducts = async () => {
+    const apiproducts = await Getproductsapi();
+    setProducts(apiproducts);
+  };
+  useEffect(() => {
+    getproducts();
+  }, []);
+
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="search brand"
+        className="setside mt-3"
+        onChange={(ev) => setSearch(ev.target.value)}
+        value={search}
+      />
+      <div className="container mt-4">
+        <h2>Cars Details</h2>
+        <div className="row">
+          {products
+            .filter((productdetails) =>
+              productdetails.name.toLowerCase().includes(search.toLowerCase())
+            )
+            .map((product) => (
+              <div className="col-md-4 mb-4" key={product._id}>
+                <div className="card">
+                  <Link to={`/card/${product._id}`}>
+                    <img
+                      src={product.image}
+                      className="card-img-top"
+                      alt={product.name}
+                      width="150px"
+                      height="350px"
+                    />
+                  </Link>
+                  <div className="card-body">
+                    <h5 className="card-title text-truncate">{product.name}</h5>
+                    {/* <h5 className="card-title">Description: {product.description}</h5> */}
+                    {/* <h5 className="card-text">Price: {product.price}</h5> */}
+                    {/* <button type="button" className="btn btn-primary"  onClick={()=>{setCart([...cart,{id:product}])}}>Book Now</button>&nbsp;&nbsp; */}
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/Singleproduct.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import Getsingle from "../api/Getsingle ";
+import { useParams } from "react-router-dom";
+
+export default function Singleproduct() {
+  const [car, setCar] = useState({});
+  const params = useParams();
+  const getproducts = async () => {
+    const apiproducts = await Getsingle(params.id);
+    setCar(apiproducts);
+  };
+  useEffect(() => {
+    getproducts();
+    // eslint-disable-next-line
+  }, []);
+  
+  return (
+    <>
+     { car && <div className="car">
+        <img
+          src={car.image}
+          className="img-side"
+          alt="car"
+        />
+        <div>
+          <div className="car-body">
+            <h2>{car.name}</h2>
+            <h4>
+            {car.description}
+            </h4>
+            <h5>&#8377; {car.price}.00/-</h5>
+          </div>
+        </div>
+      </div>}
+    </>
+  );
+}
